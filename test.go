067d09b70cbed8cc9085a2d5326c3b898db15480
RobWC <rwcameron@gmail.com>
2015-05-21 13:13:47 -0700

    adding some more example lines. sleep becomes me

--- test.go
+++ test.go
@@ -10,7 +10,13 @@ func main() {
 	fmt.Println("Hello Wolrd Emoji!")
 
 	emoji.Println(":beer: Beer!!!")
-	emoji.Println(":chicken: :poultry_leg: :chicken: :poultry_leg: :chicken: :poultry_leg: :chicken:")
+	emoji.Println(":chicken: :poultry_leg: :chicken: :poultry_leg: :chicken: :nut_and_bolt: :poultry_leg: :chicken:")
+	emoji.Println(":department_store:")
+	emoji.Println(":angry:")
+	emoji.Println(":rage:")
+	emoji.Println(":u7a7a:")
+	emoji.Println(":scream_cat:")
+	emoji.Println(":radio_button:")
 
 	pizzaMessage := emoji.Sprint("I like a :pizza: and :sushi:!!")
 	fmt.Println(pizzaMessage)
