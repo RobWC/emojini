55375f18b22a856212aaef164163f5e1ffa0b419
RobWC <rwcameron@gmail.com>
2015-05-21 13:04:22 -0700

    add silliness

---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji"
+)
+
+func main() {
+	fmt.Println("Hello Wolrd Emoji!")
+
+	emoji.Println(":beer: Beer!!!")
+	emoji.Println(":chicken: :poultry_leg: :chicken: :poultry_leg: :chicken: :poultry_leg: :chicken:")
+
+	pizzaMessage := emoji.Sprint("I like a :pizza: and :sushi:!!")
+	fmt.Println(pizzaMessage)
+}
